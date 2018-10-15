43c6597edf0750c5efa8f7edc0d2b60489561fff
romainLoire <romain.loire@ynov.com>
2018-10-15 13:03:05 +0200

    début du form etweet

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,21 +5,42 @@ namespace App\Controller;
 use App\Entity\ETweet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(Request $request)
     {
         $eTweets = $this->getDoctrine()
             ->getRepository(ETweet::class)
             ->findAll();
 
-        return $this->render('homepage/homepage.html.twig', [
-            'controller_name' => 'HomepageController',
-            'eTweets' => $eTweets
-        ]);
+            $etweet = new ETweet();
+            $form = $this->createFormBuilder($etweet)
+              ->add('content', TextType::class)
+              ->add('submit', SubmitType::class, array('label' => 'ETweet'))
+              ->getForm();
+
+              $form->handleRequest($request);
+              if ($form->isSubmitted() && $form->isValid()) {
+
+     $etweet = $form->getData();
+     $etweet->setDating(new DateTime());
+     $etweet->setTeam();
+     $entityManager = $this->getDoctrine()->getManager();
+     $entityManager->persist($etweet);
+     $entityManager->flush();
+
+ }
+
+         return $this->render('homepage/homepage.html.twig', array(
+             'formETweet' => $form->createView(),
+             'eTweets' => $eTweets
+         ));
     }
 }
