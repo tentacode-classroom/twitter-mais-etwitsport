2882cdbd4dec760d7732668c284f6ebd9c6b0bef
romainLoire <romain.loire@ynov.com>
2018-10-19 15:08:51 +0200

    debut install command

--- src/Command/AppInstallCommand.php
+++ src/Command/AppInstallCommand.php
@@ -16,16 +16,16 @@ class AppInstallCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Command to install the ETwitter Project')
+            //->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            //->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        //$arg1 = $input->getArgument('arg1');
 
         if ($arg1) {
             $io->note(sprintf('You passed an argument: %s', $arg1));
